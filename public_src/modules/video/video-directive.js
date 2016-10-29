ada0c450a79dd182bbf873a5b688d84e796eb594
Chris Bruford <chrisbruford@gmail.com>
2016-10-29 23:13:38 +0100

    removed canvas
    No use for video on canvas so removed the canvas entirely. Also ammended styles so that video is not stretched vertically.

--- public_src/modules/video/video-directive.js
+++ public_src/modules/video/video-directive.js
@@ -2,46 +2,26 @@
 module.exports = function(){
 
     function link(scope, element, attrs, controller) {
-
-        const canvas = element[0].querySelector('canvas');
-        const context = canvas.getContext('2d');
         
+        //setup video
+        const video = element[0].querySelector('video');
+        video.volume = 0.0;
+        video.src = scope.src;
+
         //buttons
         const muteBtn = element[0].querySelector('.mute');
         const playBtn = element[0].querySelector('.fi-play');
         const stopBtn = element[0].querySelector('.fi-stop');
         const refreshBtn = element[0].querySelector('.fi-refresh');
 
-        //create video element
-        const video = document.createElement('video');
-        video.volume = 0.0;
-        video.src = scope.src;
-        video.autoplay = true;
-        video.autostart = true;
-
-        //play video to canvas
-        video.addEventListener('loadedmetadata', function() {
-            canvas.width = video.videoWidth;
-            canvas.height = video.videoHeight;
-        });
-
+        //event listeners
         video.addEventListener('play',() => {
-            (function draw(){
-                if (video.paused || video.ended) {
-                    return;
-                }
-                context.drawImage(video,0,0);
-
-                requestAnimationFrame(draw);
-            })();
-
             playBtn.style.color = 'grey';
             playBtn.style.transform = 'scale(1.0)';
             stopBtn.style.color = '';
             stopBtn.style.transform = '';
         })
 
-        //button event listeners
         muteBtn.addEventListener('click',()=>{
             muteBtn.classList.toggle('fi-volume-strike');
             muteBtn.classList.toggle('fi-volume');

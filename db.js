e92e1e946af15da0c9d896aef94d1861ce3a841e
Chris Bruford <chrisbruford@gmail.com>
2016-11-13 14:46:44 +0000

    hardcoding db to debug

--- db.js
+++ db.js
@@ -6,7 +6,8 @@ let secrets = require('./secrets');
 let user = process.env.db_uname || secrets.db_user;
 let pwd = process.env.db_pwd || secrets.db_pwd;
 
-let url = process.env.dbURL || "mongodb://localhost:27017/kok_db";
+//let url = process.env.dbURL || "mongodb://localhost:27017/kok_db";
+let url = "mongodb://bitnami-mongodb-4d21.cloudapp.net:27017/kok_db";
 
 module.exports = new Promise((resolve, reject) => {
     mongoose.connect(url, { user: user, pass: pwd },

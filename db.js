7d40f8c0cab9b10dacb09724cbfba32e8fc0d183
Chris Bruford <chrisbruford@gmail.com>
2016-11-13 13:51:03 +0000

    added env var to handle DB

--- db.js
+++ db.js
@@ -3,10 +3,10 @@
 let mongoose = require('mongoose');
 let secrets = require('./secrets');
 
-let user = secrets.db_user;
-let pwd = secrets.db_pwd;
+let user = process.env.db_uname || secrets.db_user;
+let pwd = process.env.db_pwd || secrets.db_pwd;
 
-let url = "mongodb://localhost:27017/kok_db";
+let url = process.env.dbURL || "mongodb://localhost:27017/kok_db";
 
 module.exports = new Promise((resolve, reject) => {
     mongoose.connect(url, { user: user, pass: pwd },

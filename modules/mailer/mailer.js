c199c7bfcfa13c77e80514100cf46f9187bf3b1a
Chris Bruford <chrisbruford@gmail.com>
2017-01-09 22:27:11 +0000

    chore: moved api key to env_var/secret
    Also setup fallbacks so it works on localhost.

--- modules/mailer/mailer.js
+++ modules/mailer/mailer.js
@@ -1,12 +1,13 @@
 "use strict";
 let nodemailer = require('nodemailer');
 let mg = require('nodemailer-mailgun-transport');
+let secrets = require('../../secrets.js');
+
+let api_key = process.env.nm_api_key || secrets.nodemailer.api_key;
+let domain = process.env.nm_domain || secrets.nodemailer.domain;
 
 let auth = {
-  auth: {
-    api_key: 'key-57e370414573a1093dd38cf867df4544',
-    domain: 'mail.knightsofkarma.com'
-  }
+  auth: {api_key,domain}
 }
 
 let nodemailerMailgun = nodemailer.createTransport(mg(auth));
--- routes/activate.js
+++ routes/activate.js
@@ -3,7 +3,6 @@ let express = require('express');
 let router = express.Router();
 let passport = require('passport');
 
-<<<<<<< HEAD
 router.get('/:token', function (req, res) {
     require('../models/user')
         .then(User => {
@@ -34,21 +33,6 @@ router.get('/:token', function (req, res) {
             console.log('activation error:', err)
             res.json(false);
         })
-=======
-router.get('/:token',function(req,res){
-    require('../models/user')
-    .then(User => {
-        let token = req.params.token;
-        return User.findOneAndUpdate({token},{activated:true,token:''})
-    })
-    .then(User => {
-        if (User) res.json(true);
-        else res.json(false);
-    })
-    .catch(err=>{
-        res.json(false);
-    })
->>>>>>> 031648a55ee930e0aea7dfa2aed42311fef3ad2e
 })
 
 module.exports = router;

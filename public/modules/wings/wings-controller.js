a1e49a8cc8ecee09258e75b214251f4c1437ef64
Chris Bruford <chrisbruford@gmail.com>
2017-01-14 19:25:44 +0000

    feat: started join wing work
    added button to template which calls joinWing on ctrl

--- public/modules/wings/wings-controller.js
+++ public/modules/wings/wings-controller.js
@@ -2,4 +2,14 @@
 module.exports = function(wings) {
     let vm = this;
     vm.wings = wings;
+    vm.joinStatus = {};
+    
+    vm.wings.forEach((wing)=>{
+        vm.joinStatus[wing.name] = null;
+    })
+
+    vm.joinWing = (wing) => {
+        console.log(`joining wing ${wing.name}`);
+        vm.joinStatus[wing.name] = "loading";
+    }
 }

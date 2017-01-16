30c347594e0870bfdba9fa68f9e18b6630131bd8
Chris Bruford <chrisbruford@gmail.com>
2017-01-16 22:01:25 +0000

    feat: adds password change

--- routes/resetPassword.js
+++ routes/resetPassword.js
@@ -3,64 +3,87 @@ let express = require('express');
 let router = express.Router();
 let passport = require('passport');
 
-router.post('/newpass',function(req,res){
+router.post('/newpass', function (req, res, next) {
+
+    let token = req.body.token;
+    let user = req.user;
+
     //pull in models and then find the user that
     //matches the username found in the Token document
-    Promise.all([require('../models/recoveryToken'),require('../models/user')])
-    .then(array=>{
-        console.log('you lose the bet big boy');
-        let recoveryToken = array[0];
-        let User = array[1];
-        let token = req.body.token;
-        recoveryToken.findOne({ uuid: token })
-        .then(token => {
-            if (token) {
-                User.findOne({ username: token.username })
-                .then(user => {
-                    if (user) {
-                        //user and token found
-                        new Promise((resolve,reject)=>{
-                            user.setPassword(req.body.newPass,err=>{
-                                if (err) {reject(err)}
-                                else {resolve(user)}
-                            })
+    Promise.all([require('../models/recoveryToken'), require('../models/user')])
+        .then(array => {
+            let recoveryToken = array[0];
+            let User = array[1];
+
+            //change password if user is logged in
+            if (user) {
+                User.findOne(user)
+                    .then(user => changePassword(user))
+                    .then(() => {return});
+            }
+
+            //otherwise try and change using token
+            if (token && token !== "undefined") {
+                recoveryToken.findOne({ uuid: token })
+                    .then(token => {
+                        if (token) {
+                            User.findOne({ username: token.username })
+                                .then(user => changePassword(user,token))
+                                .catch(err => {
+                                    console.log('user error:', err);
+                                    res.sendStatus(false);
+                                })
+                        }
+                        else { //token not found
+                            res.json(false);
+                        }
+                    })
+                    .catch(err => {
+                        console.log('token error: ', err);
+                        res.json(false);
+                    })
+            }
+
+            function changePassword(user,token) {
+                if (user) {
+                    //user and/or token found
+                    new Promise((resolve, reject) => {
+                        user.setPassword(req.body.newPass, err => {
+                            if (err) { reject(err) }
+                            else { resolve(user) }
                         })
-                        .then(()=>{
-                            Promise.all(user.save(),token.remove());
+                    })
+                        .then(() => {
+                            user.save()
+                            .then(()=>{
+                                if (token) token.remove()
+                            });
                         })
-                        .then(()=>res.json(true))
-                        .catch(err=>{
+                        .then(() => res.json(true))
+                        .catch(err => {
                             console.log(err);
                             res.json(false);
                         });
-                    } else {
-                        //matching user not found
-                        res.json(false);
-                    }
-                })
-                .catch(err => {
-                    console.log('user error:', err);
-                    res.sendStatus(false);
-                })
-            }
-            else { //token not found
-                res.json(false);
+                } else {
+                    //matching user not found
+                    res.json(false);
+                }
             }
         })
         .catch(err => {
-            console.log('token error: ', err);
+            console.log('model error:', err);
             res.json(false);
         })
-    })
-    .catch(err => {
-        console.log('model error:', err);
-        res.json(false);
-    })
 });
 
-router.get('/:token', function (req, res) {
+router.get('/:token?', function (req, res, next) {
     //pull in models and then find the user that
     //matches the username found in the Token document
+
+    if (!req.params.token || req.params.token === "undefined") {
+        return next();
+    };
+
     require('../models/recoveryToken').then(
         recoveryToken => {
             require('../models/user')
@@ -68,20 +91,20 @@ router.get('/:token', function (req, res) {
                     let token = req.params.token;
                     recoveryToken.findOne({ uuid: token }).then(token => {
                         if (token) {
-                        User.findOne({ username: token.username })
-                            .then(user => {
-                                if (user) {
-                                    //user and token found
-                                    res.json(true);
-                                } else {
-                                    //matching user not found
-                                    res.json(false);
-                                }
-                            })
-                            .catch(err => {
-                                console.log('user error:', err);
-                                throw err;
-                            })
+                            User.findOne({ username: token.username })
+                                .then(user => {
+                                    if (user) {
+                                        //user and token found
+                                        res.json(true);
+                                    } else {
+                                        //matching user not found
+                                        res.json(false);
+                                    }
+                                })
+                                .catch(err => {
+                                    console.log('user error:', err);
+                                    throw err;
+                                })
                         }
                         else { //token not found
                             res.json(false);
@@ -101,6 +124,27 @@ router.get('/:token', function (req, res) {
             console.log('token error:', err);
             //I'm a teapot
             res.sendStatus(418);
+        });
+}, function (req, res, next) {
+    //pull in models and then find the user that
+    //matches the username found in the Token document
+    let user = req.user;
+
+    if (!user) {
+        res.sendStatus(401);
+        return;
+    }
+
+    require('../models/user')
+        .then(User => {
+            return User.findOne(user);
+        })
+        .then(user => {
+            if (user) res.json(true);
+        })
+        .catch(err => {
+            console.log(err);
+            res.json(false);
         });
 });
 

f2f17559c6526ad33be721caab2d5a03f6574d16
Sayak Mukhopadhyay <mukhopadhyaysayak@gmail.com>
2017-01-29 21:30:34 +0530

    Angular updated to 1.6.1 and files corrected to handle the new hash prefix

--- public/assets/es6/app.js
+++ public/assets/es6/app.js
@@ -6,54 +6,59 @@
     let angular = require('angular');
     require('angular-route');
     require('angular-messages');
+    require('angular-animate');
     require('ng-file-upload');
+    require('ng-image-gallery');
 
-    angular.module('kokApp', ['ngRoute','ngMessages','ngFileUpload'])
-        .run(['$document','$rootScope',function ($document,$rootScope) {
-                $($document).foundation();
+    angular.module('kokApp', ['ngRoute', 'ngMessages', 'ngFileUpload', 'thatisuday.ng-image-gallery'])
+        .run(['$document', '$rootScope', function ($document, $rootScope) {
+            $($document).foundation();
         }])
-        .config(['$routeProvider',function($routeProvider){
-            $routeProvider.when('/',{
-                templateUrl: 'routes/home.html',
-                controller: 'RouteCtrl',
-                routePage: 'Home'
-            })
-            .when('/contact',{
-                templateUrl: 'routes/contact.html',
-                controller: 'RouteCtrl',
-                routePage: 'Contact'
-            })
-            .when('/roster',{
-                templateUrl: 'routes/roster.html',
-                controller: 'RouteCtrl',
-                routePage: 'Roster'
-            })
-            .when('/gallery',{
-                templateUrl: 'routes/gallery.html',
-                controller: 'RouteCtrl',
-                routePage: 'Gallery'
-            })
-            .when('/wings',{
-                templateUrl: 'routes/wings.html',
-                controller: 'RouteCtrl',
-                routePage: 'Wings'
-            })
-            .when('/account',{
-                templateUrl: 'routes/account.html',
-                controller: 'RouteCtrl',
-                routePage: 'Account'
-            })
-            .when('/admin',{
-                templateUrl: 'routes/admin.html',
-                controller: 'RouteCtrl',
-                routePage: 'Admin'
-            })
-            .when('/activate/:token',{
-                templateUrl: 'routes/activate.html'
-            })
-            .when('/resetpassword/:token',{
-                templateUrl: 'routes/resetpassword.html'
-            })
+        .config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
+            $routeProvider
+                .when('/', {
+                    templateUrl: 'routes/home.html',
+                    controller: 'RouteCtrl',
+                    routePage: 'Home'
+                })
+                .when('/contact', {
+                    templateUrl: 'routes/contact.html',
+                    controller: 'RouteCtrl',
+                    routePage: 'Contact'
+                })
+                .when('/roster', {
+                    templateUrl: 'routes/roster.html',
+                    controller: 'RouteCtrl',
+                    routePage: 'Roster'
+                })
+                .when('/gallery', {
+                    templateUrl: 'routes/gallery.html',
+                    controller: 'RouteCtrl',
+                    routePage: 'Gallery'
+                })
+                .when('/wings', {
+                    templateUrl: 'routes/wings.html',
+                    controller: 'RouteCtrl',
+                    routePage: 'Wings'
+                })
+                .when('/account', {
+                    templateUrl: 'routes/account.html',
+                    controller: 'RouteCtrl',
+                    routePage: 'Account'
+                })
+                .when('/admin', {
+                    templateUrl: 'routes/admin.html',
+                    controller: 'RouteCtrl',
+                    routePage: 'Admin'
+                })
+                .when('/activate/:token', {
+                    templateUrl: 'routes/activate.html'
+                })
+                .when('/resetpassword/:token', {
+                    templateUrl: 'routes/resetpassword.html'
+                });
+            $locationProvider.hashPrefix('');
+            $locationProvider.html5Mode(true);
         }]);
 })();
 

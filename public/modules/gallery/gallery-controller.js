1b696616118432fa0532fb1dc0d47406f1a0440e
Sayak Mukhopadhyay <mukhopadhyaysayak@gmail.com>
2017-02-02 00:36:23 +0530

    Added an access denied page and hidden upload bar on unlogging

--- public/modules/gallery/gallery-controller.js
+++ public/modules/gallery/gallery-controller.js
@@ -1,12 +1,15 @@
 "use strict";
-module.exports = function (DataService, $scope) {
+module.exports = function (DataService, AuthService, $scope) {
+    let vm = this;
+
+    vm.user = AuthService.user;
+
+    $scope.$on('authenticated', event => vm.user = AuthService.user);
+    $scope.$on('deauthenticated', event => vm.user = AuthService.user);
+
     let data = DataService.getGallery();
     data.then(data => {
         console.log(data);
         $scope.images = data;
     });
-
-    this.selectImage = (image) => {
-        this.selectedImage = image;
-    }
 }
--- public/modules/gallery/index.js
+++ public/modules/gallery/index.js
@@ -1,4 +1,4 @@
 "use strict";
 angular.module('kokApp')
-    .controller('GalleryCtrl', ['DataService', '$scope', require('./gallery-controller')])
+    .controller('GalleryCtrl', ['DataService', 'AuthService', '$scope', require('./gallery-controller')])
     .directive('kokGallery', [require('./gallery-directive')]);

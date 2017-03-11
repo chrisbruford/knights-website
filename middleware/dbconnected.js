bb72ce24613200e5ae9487fb3c571d4eb54a115d
Chris Bruford <chrisbruford@gmail.com>
2017-03-11 17:08:46 +0000

    fix: removed connection loop

--- middleware/dbconnected.js
+++ middleware/dbconnected.js
@@ -1,10 +1,19 @@
 "use strict";
 const db = require('../db.js');
 
+let tracker =  0;
+
 module.exports = (req,res,next) => {
     if (db.mongoose.connection.status === 0) {
         next();
-    } else {
+    } else if (tracker < 5) {
+
+        tracker++;
+
+        setTimeout(()=>{
+            tracker--;
+        },60000)
+
         db.connect();
         next();
     }

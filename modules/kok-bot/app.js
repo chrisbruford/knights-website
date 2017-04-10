3dcd1e5b5543736340fcb5fdc1efad46393b59fb
Chris Bruford <chrisbruford@gmail.com>
2017-04-10 21:45:08 +0100

    fix: only initialise bot cmds and events once its ready

--- modules/kok-bot/app.js
+++ modules/kok-bot/app.js
@@ -9,11 +9,32 @@ var guild;
 
 //initialise
 client.on("ready", () => {
-    console.log('kokbot logged in');
+    console.log('kokbot ready');
     guild = client.guilds.get(kokGuildID);
+
+    //commands
+    const commands = require('./modules/common/commands');
+    commands.initiateCommands();
+
+    //events
+    require('./modules/common/events');
+
+    //event listeners
+    const wings = require('./modules/wings')
+    const wingController = require('../../controllers/wingController');
+
+    wingController.on('joinWing', (evt) => {
+        let member = guild.members.get(evt.user.discordID);
+        wings.joinWing(evt.wingName, member);
+    })
+
+    wingController.on('leaveWing', (evt) => {
+        let member = guild.members.get(evt.user.discordID);
+        wings.leaveWing(evt.wingName, member);
+    })
 });
 
-client.on("reconnecting",()=>{
+client.on("reconnecting", () => {
     console.log('kokbot reconnecting');
 })
 
@@ -28,27 +49,6 @@ client.login(token)
     .catch(err => {
         console.log('login error:', err);
     })
-
-//commands
-const commands = require('./modules/common/commands');
-commands.initiateCommands();
-
-//events
-require('./modules/common/events');
-
-//event listeners
-const wings = require('./modules/wings')
-const wingController = require('../../controllers/wingController');
-
-wingController.on('joinWing', (evt) => {
-    let member = guild.members.get(evt.user.discordID);
-    wings.joinWing(evt.wingName, member);
-})
-
-wingController.on('leaveWing', (evt) => {
-    let member = guild.members.get(evt.user.discordID);
-    wings.leaveWing(evt.wingName, member);
-})
 
 //modules
 let register = require('./modules/register');

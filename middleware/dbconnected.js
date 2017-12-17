d0b42211bfa5053079671044133122cf19cc84b5
Chris Bruford <chrisbruford@gmail.com>
2017-12-17 23:55:28 +0000

    fix: correctly check for connection state
    dbconnected was checking the wrong property for the wrong value and so causing unnecessary connection attempts.

--- middleware/dbconnected.js
+++ middleware/dbconnected.js
@@ -4,10 +4,9 @@ const db = require('../db.js');
 let tracker =  0;
 
 module.exports = (req,res,next) => {
-    if (db.mongoose.connection.status === 0) {
+    if (db.mongoose.connection.readyState === 1) {
         next();
     } else if (tracker < 5) {
-
         tracker++;
 
         setTimeout(()=>{

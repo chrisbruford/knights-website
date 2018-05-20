70e6858c0977ade0e9578a4c1579df88b49d6ceb
Chris Bruford <chrisbruford@gmail.com>
2018-05-20 16:35:31 +0100

    fix: need to handle XFF for IISNode

--- middleware/ip-monitor.js
+++ middleware/ip-monitor.js
@@ -4,10 +4,11 @@ let tracker =  0;
 
 const IP_MAP = new Map();
 const MAX_REQUESTS = 100;
-const MAX_TIME = 1000;
+const MAX_TIME = 3000;
 
 module.exports = (req,res,next) => {
-    const IP = req.ip || req.connection.remoteAddress;
+    const IP = req.ip || req.connection.remoteAddress || req.headers['x-forwarded-for'];
+    LOGGER.log(`Request from ${IP}`);
     //if no IP then connection already gone, so ditch it
     if (!IP) return;
     let hits = IP_MAP.get(IP);
@@ -21,6 +22,7 @@ module.exports = (req,res,next) => {
         IP_MAP.set(IP,++hits);
         setTimeout(decrement, MAX_TIME, IP);
         if (hits >= MAX_REQUESTS) {
+            console.log(`Ignoring ${IP} due to too many requests`);
             return
         } else {
             next();

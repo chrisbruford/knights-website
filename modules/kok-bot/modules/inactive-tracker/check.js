0214e30a862bc740a75880ffa2bd397f88b9352d
Chris Bruford <chrisbruford@gmail.com>
2018-05-28 03:17:40 +0100

    chore: remove console logs

--- modules/kok-bot/modules/inactive-tracker/check.js
+++ modules/kok-bot/modules/inactive-tracker/check.js
@@ -53,10 +53,8 @@ module.exports = (member, maxAge) => {
                 if (timeAsMember > maxAge) {
                     //not spoken since this function was implemented
                     //and been in server for longer than maxAge
-                    console.log(`marking member ${member.user.username} who has never spoken`);
                     return mark(member);
                 } else {
-                    console.log(`not marking new member ${member.user.username}`);
                     return;
                 }
             }
@@ -65,10 +63,8 @@ module.exports = (member, maxAge) => {
             let elapsedTime = userLog.lastMessage ? Date.now() - userLog.lastMessage : Infinity;
 
             if (elapsedTime > maxAge) {
-                console.log(`marking member ${member.user.username}`);
                 return mark(member);
             } else {
-                console.log(`unmarking member ${member.user.username}`);
                 return unmark(member);
             }
         })

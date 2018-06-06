a86a9c1cbc4871fb4e774be68680cb45016305dc
Chris Bruford <chrisbruford@gmail.com>
2018-06-06 20:43:10 +0100

    fix: lowercase username for existance check

--- public/modules/common/validators/cmdrName-directive.js
+++ public/modules/common/validators/cmdrName-directive.js
@@ -3,7 +3,7 @@ module.exports = function(DataService,$q) {
 
     function link(scope,elem,attr,ctrl){
         ctrl.$asyncValidators.cmdrName = (modelVal, viewVal) => {
-            return DataService.getMembersByUsername(modelVal)
+            return DataService.getMembersByUsername(modelVal.toLowerCase())
             .then(members=>{
                 return $q((resolve,reject)=>{
                     if (members.length > 0) {

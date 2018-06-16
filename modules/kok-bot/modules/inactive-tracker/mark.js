b269c8478bbe179d49d223ad5f22b5668891a2b9
Chris Bruford <chrisbruford@gmail.com>
2018-06-16 11:04:47 +0100

    feat: mark user's profiles as active/inactive

--- modules/kok-bot/modules/inactive-tracker/mark.js
+++ modules/kok-bot/modules/inactive-tracker/mark.js
@@ -1,16 +1,15 @@
 const GuildController = require('../controllers/guild-controller');
 const discordUsers = require('../../../../models/discord-users');
-const logger = require('../../../logger')
+const User = require('../../../../models/user');
+const logger = require('../../../logger');
 
 module.exports = (member) => {
     return GuildController.find(member.guild.id)
-        //finds guild model and handles fail conditions
         .then(guild => {
             if (!guild) { return logger.log('no such guild found') }
             if (!guild.inactiveRole) { return logger.log('no inactiveRole set') }
             //checks each role on member if it's an Activity Role
             //if it is, then it is removed and a record kept on their entry in the DB
-
             discordUsers.findOneOrCreate({ guildID: member.guild.id }, { guildID: member.guild.id })
                 .then(usersGuild => {
                     //create collection of Active Roles to be removed and recorded
@@ -20,12 +19,12 @@ module.exports = (member) => {
                             activityRoles.push(role.id);
                             member.removeRole(role.id);
                         }
-                    })
+                    });
 
                     //looks for this user amongst all the users
                     let foundUser = usersGuild.users.find(user => {
                         return user.id === member.id;
-                    })
+                    });
                     //if they're not found then they should be added
                     if (!foundUser) {
                         usersGuild.users.addToSet({
@@ -39,11 +38,14 @@ module.exports = (member) => {
                     }
 
                     usersGuild.save();
-                })
+                });
+
+            User.findOneAndUpdate({discordID: member.id},{active: false})
+                .catch(logger.log);
 
             return member.addRole(guild.inactiveRole);
         })
         .catch(err => {
             logger.log(err);
-        })
+        });
 }

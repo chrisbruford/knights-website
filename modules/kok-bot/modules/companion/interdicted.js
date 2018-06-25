095d652056fe83c11acd30399aba7c3251f63deb
Chris Bruford <chrisbruford@gmail.com>
2018-06-25 19:39:12 +0100

    style: use embeds for interdiction alerts

--- modules/kok-bot/modules/companion/interdicted.js
+++ modules/kok-bot/modules/companion/interdicted.js
@@ -1,11 +1,17 @@
 const broadcast = require('../broadcasts/broadcast');
 const logger = require('../../../logger');
+const Discord = require('discord.js');
 
 function alert(user, interdictedEvt, system) {
 
-    let message = `INTERDICTION ALERT: ${user.username.toUpperCase()} interdicted by ${interdictedEvt.Interdictor} in ${system}`
+    let embed = new Discord.RichEmbed();
+    embed.setColor(16711680);
+    embed.setTitle(`🚨 INTERDICTION ALERT 🚨`);
+    embed.addField(`CMDR in Danger`, user.username.toUpperCase(),true);
+    embed.addField(`Interdictor`,interdictedEvt.Interdictor.toUpperCase(), true);
+    embed.addField('System', system);
 
-    return broadcast(user,message)
+    return broadcast(user,embed)
         .catch(err=>{
             logger.log(err);
             return Promise.reject(err);

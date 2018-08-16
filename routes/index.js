92848da6db0c0a145d9b3d8ab499ca8b08442808
Chris Bruford <chrisbruford@gmail.com>
2018-08-16 16:18:53 +0100

    feat: add path for companion app download

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,23 @@
 let express = require('express');
 let router = express.Router();
 let path = require('path');
+let fs = require('fs');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.sendFile(path.resolve(__dirname, `../public/index.html`));
+router.get('/', function (req, res, next) {
+    res.sendFile(path.resolve(__dirname, `../public/index.html`));
 });
+
+router.get('/companion', function (req, res, next) {
+    fs.readdir(path.resolve(__dirname, '../public'), (err, filepaths) => {
+        if (!filepaths) { res.sendStatus(404) };
+        if (err) { res.sendStatus(500) };
+
+        let appPath = filepaths.filter(path=>/^knight-companion Setup [0-9].[0-9].[0-9].exe$/.test(path)).sort().pop();
+
+        if (!appPath) { res.sendStatus(404) }
+        else { res.download(path.resolve(__dirname, `../public/${appPath}`),`knight companion setup.exe`) }
+    });
+})
 
 module.exports = router;

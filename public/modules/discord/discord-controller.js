67be2ae2326343cf80a9c47f39bd5713cfbe651b
Sayak Mukhopadhyay <mukhopadhyaysayak@gmail.com>
2019-06-01 14:45:12 +0530

    Removed all instances of Discord invite links and replaced with info

--- public/modules/discord/discord-controller.js
+++ public/modules/discord/discord-controller.js
@@ -1,9 +1,14 @@
 "use strict";
 
-module.exports = function(DataService){
+module.exports = function (DataService) {
     let discordData = DataService.getDiscordData();
 
-    discordData.then((data)=>{
-        this.members = data.members;
+    discordData.then((data) => {
+        this.members = [];
+        data.members.forEach(member => {
+            if (!member.bot) {
+                this.members.push(member);
+            }
+        });
     });
 };
